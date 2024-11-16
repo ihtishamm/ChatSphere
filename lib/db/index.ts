48a7b1b35457f2fdf17d89ec3b57b3b244970119
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2024-11-16 11:20:38 -0800

    Add the schema and done the migrations

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,6 +1,8 @@
 import {neon, neonConfig} from '@neondatabase/serverless'
+import { config } from "dotenv";
 import { drizzle } from 'drizzle-orm/neon-http';
 neonConfig.fetchConnectionCache = true
+config({ path: ".env" }); 
 
 if (!process.env.DATABASE_UR){
     throw new Error('DATABASE url not found')
@@ -10,7 +12,7 @@ const databaseUrl = process.env.DATABASE_URL;
 if (!databaseUrl) {
     throw new Error('DATABASE_URL is not defined');
 }
-const sql = neon(databaseUrl);
+const sql = neon(databaseUrl!);
 
 
 export const db = drizzle(sql)
