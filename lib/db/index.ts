539673b1c014d4191067aa45443b66a7351e24b9
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2024-11-16 10:56:12 -0800

    configure the neon database and fix the drizzle installation issue by changing the next js version from 15+ to 14.2.5

---
+++ lib/db/index.ts
@@ -0,0 +1,16 @@
+import {neon, neonConfig} from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/neon-http';
+neonConfig.fetchConnectionCache = true
+
+if (!process.env.DATABASE_UR){
+    throw new Error('DATABASE url not found')
+}
+
+const databaseUrl = process.env.DATABASE_URL;
+if (!databaseUrl) {
+    throw new Error('DATABASE_URL is not defined');
+}
+const sql = neon(databaseUrl);
+
+
+export const db = drizzle(sql)
