29a0f4fa28dbd0361fa8c17f96be30eff4f86c12
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2025-01-18 08:58:01 -0800

    update the file management code to make sure it works on all operating systems

--- lib/s3-server.ts
+++ lib/s3-server.ts
@@ -1,49 +1,48 @@
 import fs from 'fs';
 import path from 'path';
 import AWS from 'aws-sdk';
-
+import { tmpdir } from 'os';
 
 export const downloadFromS3 = async (file_key: string) => {
-
     try {
-
+    
         AWS.config.update({
-           accessKeyId:process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
-           secretAccessKey:process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
+            accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
+            secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
         });
+
         const s3 = new AWS.S3({
-         params:{
-               Bucket:process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
-         },
-         region:'eu-north-1',
+            region: 'eu-north-1',
         });
-         
+
         const bucketName = process.env.NEXT_PUBLIC_S3_BUCKET_NAME;
         if (!bucketName) {
-            throw new Error("Bucket name is not defined");
+            throw new Error('Bucket name is not defined');
         }
 
         const params = {
-           Bucket: bucketName,
-            Key: file_key
-        }
+            Bucket: bucketName,
+            Key: file_key,
+        };
+
         const obj = await s3.getObject(params).promise();
-    const tempDir = path.join('C:', 'temp');
-    const fileName = path.join(tempDir, `pdf-${Date.now()}.pdf`);
 
-    // Ensure the temp directory  exists
-    if (!fs.existsSync(tempDir)) {
-        fs.mkdirSync(tempDir, { recursive: true });
-    }
+        
+        const tempDir = path.join(tmpdir(), 'chatsphere');
+        const fileName = path.join(tempDir, `pdf-${Date.now()}.pdf`);
 
-    console.log('file_name', fileName);
-    fs.writeFileSync(fileName, obj.Body as Buffer);
-    return fileName; 
+        if (!fs.existsSync(tempDir)) {
+            fs.mkdirSync(tempDir, { recursive: true });
+        }
+
+        console.log('File path:', fileName);
 
       
-           
+        fs.writeFileSync(fileName, obj.Body as Buffer);
+
+        return fileName;
     } catch (error) {
-       console.log('error uploading to s3',error)
+        console.error('Error downloading from S3:', error);
+        throw error; 
     }
-    
 };
